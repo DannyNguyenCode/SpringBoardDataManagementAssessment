1389a441e57b63a87da77094d58a8c5faa656389
Danny <giabnguyen1@gmail.com>
2022-08-30 16:15:43 -0400

    Added instructions on how to get to source code/run code on localhost. Also added comments in source code.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SpringBoardDataManagementAssessment
+
+
+### Source Code File Directory
+
+* spring-board-assessment/src/utility/findNumberGreaterThan.js
+
+### How to Run Program on localhost
+
+* git clone https://github.com/DannyNguyenCode/SpringBoardDataManagementAssessment.git
+* open folder in Visual Studio Code
+* cd spring-board-assessment
+* npm install
+* npm start
--- spring-board-assessment/src/utility/findNumberGreaterThan.js
+++ spring-board-assessment/src/utility/findNumberGreaterThan.js
@@ -5,20 +5,26 @@ let getNumberGreaterThan16ToPower4 = ()=>{
     let result;
     let i = min;
     let answer;
+    // loop through the power starting from 5 to 256;
     while(i >= min && i <= max){
         let numberToString = "";
         let arrayOfNumbers = [];
+        
+        // get the results of 16 to the power of i
         result = Math.pow(16,i);
         numberToString = result.toString(10);
 
+        // check if results returns a number or infinity
         if(!isFinite(numberToString)) {
             answer = `There was no base 10 number that resulted from a power greater than 16^4 that excluded digits 1, 2, 4, or 8. The returned value was ${numberToString}` ;
             break;
         }
+        // if results return a number loop through string and add each number to array
         for(let x = 0; x < numberToString.length; x++){
             arrayOfNumbers.push(Number(numberToString[x]));
         }
-
+        
+        // compare array of numbers with the numbers to be excluded (1,2,4,8)
         result = numbersExcluded.filter((n)=>{
             return arrayOfNumbers.indexOf(n) !== -1; 
         })
